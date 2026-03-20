7d293a55c7b386301a972ac0c093c8c1c7708d67
DaniDev34 <daniel.chaparro.resendiz1@gmail.com>
2026-03-20 04:45:23 -0600

    Actividad terminada, ejercicios faltantes

---
+++ booleantest.py
@@ -0,0 +1,9 @@
+a = True
+b = False
+c = True
+
+print(a and b)          # False
+print(a or b)           # True
+print(not b)            # True
+print(a and c)          # True
+print((a or b) and c)   # True
