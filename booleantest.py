199e31e1fb3850ba7a2752e45b19c4978756cc65
DaniDev34 <daniel.chaparro.resendiz1@gmail.com>
2026-03-26 09:14:13 -0600

    limpieza rama 2

--- booleantest.py
+++
@@ -1,9 +0,0 @@
-a = True
-b = False
-c = True
-
-print(a and b)          # False
-print(a or b)           # True
-print(not b)            # True
-print(a and c)          # True
-print((a or b) and c)   # True
